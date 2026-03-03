f5234337f106c4d37df07354a833b5306464ce27
Imcab <Imcab@users.noreply.github.com>
2026-03-03 05:33:17 +0000

    deploy: 2d7931d173de4e148dc3f408095b448cb916027c

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.miequipo.mifeature"},{"l":"com.miequipo.mifeature.generated"}];updateSearchResults();
---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.miequipo.mifeature.generated","l":"FeatureConstants"},{"p":"com.miequipo.mifeature","l":"FeatureMain"}];updateSearchResults();
